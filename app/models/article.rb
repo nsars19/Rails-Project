9e2329b7dca03b23b6a3752d65e0d9da8891f785
Nick <nsardonini@gmail.com>
2020-03-21 18:12:10 -0400

    add tag_list method

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
 class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
-  has_many :tags, through: :taggings    
+  has_many :tags, through: :taggings
+  
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")  
+  end
 end
