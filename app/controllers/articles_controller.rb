5f0c2b1087dbacab7d0743f36d56ab7bd6efac12
Nick <nsardonini@gmail.com>
2020-03-21 12:54:48 -0400

    add destroy to ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,4 +19,10 @@ class ArticlesController < ApplicationController
 
     redirect_to article_path(@article)
   end
+
+  def destroy
+    Article.find(params[:id]).destroy
+
+    redirect_to articles_path
+  end
 end
