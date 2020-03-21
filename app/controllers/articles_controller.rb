2b5eb2c2db4ece1f39e5c01ac61e766b666ad7a1
Nick <nsardonini@gmail.com>
2020-03-21 12:05:56 -0400

    add block to create in ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.save
+    redirect_to article_path(@article)
   end
 end
