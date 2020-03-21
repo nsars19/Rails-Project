473e53b6c3e46290bfadb012ca4e2de04d3683cc
Nick <nsardonini@gmail.com>
2020-03-21 19:37:57 -0400

    refactor delete for tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
-  has_many :tags, through: :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
   
   def tag_list
     self.tags.collect do |tag|
